b6cf519c7e2ae1bd566e6008501e27024a0678d1
An-Shine <akh1012@naver.com>
2025-11-05 15:33:37 +0900

    dd

---
+++ Assets/AngryBotResources/Scripts/Photonmanager.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Photonmanager : MonoBehaviour
+{
+    readonly string version = "1.0";
+    string userId = "AKH";
+}
