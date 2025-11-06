e3bc31e7138933a7e66531c31808e126ba5173e1
An-Shine <akh1012@naver.com>
2025-11-06 14:34:01 +0900

    edd

--- Assets/AngryBotResources/Scripts/Photonmanager.cs
+++ Assets/AngryBotResources/Scripts/Photonmanager.cs
@@ -1,9 +1,79 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
+using Photon.Realtime;
 
-public class Photonmanager : MonoBehaviour
+public class PhotonManager : MonoBehaviourPunCallbacks
 {
+    //게임의 버젼
     readonly string version = "1.0";
+    //유저 닉네임
     string userId = "AKH";
+
+    void Awake()
+    {
+        //마스터 클라이언트의 씬 자동 동기화 옵션
+        PhotonNetwork.AutomaticallySyncScene = true;
+
+        //게임의 버젼을 설정
+        PhotonNetwork.GameVersion = version;
+
+        //접속 유저의 닉네임 설정
+        PhotonNetwork.NickName = userId;
+
+        //포톤 서버와의 데이터 초당 전송횟수 설정
+        Debug.Log(PhotonNetwork.SendRate);
+
+        //포톤 서버 접속
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    //포톤 서버에 접속 후 호출되는 콜백함수
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to Master!");
+        Debug.Log($"PhotonNetwork.InLobby = {PhotonNetwork.InLobby}");
+        PhotonNetwork.JoinLobby();
+    }
+
+    //로비에접속 후 호출되는 콜백함수
+    public override void OnJoinedLobby()
+    {
+        Debug.Log($"PhotonNetwork.InLobby - {PhotonNetwork.InLobby}");
+    }
+
+    //랜덤한 룸 입장이 실패했을경우 호출되는 롤백함수
+    public override void OnJoinRandomFailed(short returnCode, string message)
+    {
+        Debug.Log($"JoinRandom Failed{returnCode} : {message}");
+
+        //룸의 속성 정의
+        RoomOptions ro = new RoomOptions();
+        ro.MaxPlayers = 20;     //입장가능한 최대접속자 수
+        ro.IsOpen = true;       //룸의 오픈 여부
+        ro.IsVisible = true;    //로비에서 룸 목록에 노출시킬지 여부
+
+    }
+
+    // 룸 생성이 완료 된 후 호출되는 콜백함수
+    public override void OnCreatedRoom()
+    {
+        Debug.Log("Created Room");
+        Debug.Log($"Room Name = {PhotonNetwork.CurrentRoom.Name}");
+    }
+
+    //룸에 입장한 후 호출되는 콜백함수
+    public override void OnJoinedRoom()
+    {
+        Debug.Log($"PhotonNetwork.InRoom = {PhotonNetwork.InRoom}");
+        Debug.Log($"Player Count = {PhotonNetwork.CurrentRoom.PlayerCount}");
+        foreach(var player in PhotonNetwork.CurrentRoom.Players)
+        {
+            Debug.Log($"{player.Value.NickName} , {player.Value.ActorNumber}");
+        }
+    }
+
 }
+
+
